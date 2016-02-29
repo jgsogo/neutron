aae73f9e3438f4b47dde9cfd25e54a23f50dd9fe
Javier G. Sogo <jgsogo@gmail.com>
2016-02-29 23:58:26 +0100

    wip

--- webapp/synthetic/views.py
+++ webapp/synthetic/views.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 from django.views.generic import UpdateView, DetailView
 from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 
+from neutron.models import Informer
 from .models import Configuration, InformerGenerated
 
 
@@ -60,3 +63,27 @@ class ConfigurationDeleteView(UpdateView):
 
 class ConfigurationDetailView(DetailView):
     model = Configuration
+    template_name = 'admin/configuration_detail.html'
+
+    def get_informers(self):
+        informers = defaultdict(lambda : defaultdict(list))
+        qs = Informer.objects.filter(informergenerated__configuration = self.object)
+        qs = InformerGenerated.objects.filter(configuration = self.object)
+        print("*"*20)
+        from django.db import models
+        print(qs.aggregate(models.Count('informer__region')))
+        for result in qs.order_by('region__name'):
+            informers[result.region]['generated'].append(result)
+        for result in self.object.regiondata_set.all():
+            informers[result.region]['theoric'].append(result.percentage)
+        return informers
+
+    def get_context_data(self, **kwargs):
+        context = super(ConfigurationDetailView, self).get_context_data(**kwargs)
+        context['site_header'] = 'Proyc'  # TODO: Incluir en el contexto los valores por defecto
+        context['title'] = _('Configuration stats details')
+        context['opts'] = Configuration._meta
+
+        context['informers'] = self.get_informers()
+
+        return context
