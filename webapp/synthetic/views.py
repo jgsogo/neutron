40a5f38f85e061bcd4a8fd401fa9cb21eab7bc4f
Javier G. Sogo <jgsogo@gmail.com>
2016-03-01 00:23:35 +0100

    basic info about informers

--- webapp/synthetic/views.py
+++ webapp/synthetic/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 from django.views.generic import UpdateView, DetailView
 from django.utils.translation import ugettext_lazy as _
@@ -9,7 +9,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 
 from neutron.models import Informer
-from .models import Configuration, InformerGenerated
+from .models import Configuration, InformerGenerated, RegionData
 
 
 class ConfigurationGenerateView(UpdateView):
@@ -62,21 +62,20 @@ class ConfigurationDeleteView(UpdateView):
 
 
 class ConfigurationDetailView(DetailView):
-    model = Configuration
     template_name = 'admin/configuration_detail.html'
 
+    def get_queryset(self):
+        return Configuration.objects.filter(generated=True)
+
     def get_informers(self):
-        informers = defaultdict(lambda : defaultdict(list))
-        qs = Informer.objects.filter(informergenerated__configuration = self.object)
-        qs = InformerGenerated.objects.filter(configuration = self.object)
-        print("*"*20)
-        from django.db import models
-        print(qs.aggregate(models.Count('informer__region')))
-        for result in qs.order_by('region__name'):
-            informers[result.region]['generated'].append(result)
-        for result in self.object.regiondata_set.all():
-            informers[result.region]['theoric'].append(result.percentage)
-        return informers
+        qs = Counter(Informer.objects.filter(informergenerated__configuration = self.object).values_list('region__name', flat=True))
+        ts = RegionData.objects.filter(configuration=self.object).values_list('region__name', 'percentage')
+        qtotal = float(sum(qs.values()))
+        data = defaultdict(lambda : [0.0, 0.0])
+        for (k1,v1),(k2,v2) in zip(qs.most_common(),ts):
+            data[k1][0] = v1/qtotal
+            data[k2][1] = v2
+        return dict(data)  # TODO: Put default_factory = None to avoid this copy
 
     def get_context_data(self, **kwargs):
         context = super(ConfigurationDetailView, self).get_context_data(**kwargs)
