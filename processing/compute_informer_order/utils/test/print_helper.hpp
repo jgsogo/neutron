8d0cf4a06a8cb657a90b4e58038c667328686538
Javier G. Sogo <jgsogo@gmail.com>
2016-04-10 20:40:15 +0200

    no return value

--- processing/compute_informer_order/utils/test/print_helper.hpp
+++ processing/compute_informer_order/utils/test/print_helper.hpp
@@ -13,7 +13,7 @@ namespace boost {
                     // TODO: Why just `os << pr` or `::operator<<(os, pr)` does not work?
                     os << "(";
                     ::utils::tuple::print_tuple_impl(os, t, std::index_sequence_for<Args...>{});
-                    return os << ")";
+                    os << ")";
                 }
             };
 
