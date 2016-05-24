dd17c788d125d114e4796f6703a6bd801a792afc
Javier G. Sogo <jgsogo@gmail.com>
2016-05-24 21:08:26 +0000

    renamed views to avoid file name clash

--- webapp/neutron/views/__init__.py
+++ webapp/neutron/views/__init__.py
@@ -1,5 +1,5 @@
 
 
-from .lemma import LemmaDetail, SearchLemma
-from .meaning import MeaningDetail, MeaningCoarsityDetail, MeaningUsesDetail
-from .telegram import NutronBotLink
+from neutron.views.lemma import LemmaDetail, SearchLemma
+from neutron.views.meaning import MeaningDetail, MeaningCoarsityDetail, MeaningUsesDetail
+from neutron.views.telegram_link import NutronBotLink
