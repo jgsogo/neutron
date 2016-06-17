9068c6aced45881bef8e235ea2347880bf8fe0d2
Javier G. Sogo <jgsogo@gmail.com>
2016-06-17 18:03:55 +0200

    some tests

--- processing/compute_informer_order/tests/basic/test_informer.cpp
+++ processing/compute_informer_order/tests/basic/test_informer.cpp
@@ -10,12 +10,30 @@ BOOST_AUTO_TEST_SUITE(informer)
 
 BOOST_AUTO_TEST_CASE(read)
 {
+    using namespace neutron;
     namespace fs = boost::filesystem;
     fs::path full_path = test_data_dir / fs::path("data_informers.tsv");
 
-    auto& informers = neutron::Informer::objects(full_path.string());
-    informers.all().count();
+    typedef qs::Model<int, std::string, float> MyModel;
+    MyModel m;
+    auto& manager = m.objects(std::string("C:/Users/xe53859/src/queryset-cpp/tests/data/ex_filequeryset.tsv"));
+    BOOST_CHECK_EQUAL(manager.all().count(), 3);
 
+
+    /*
+    typedef qs::Model<int, int, float> MyModel;
+    MyModel m;
+    auto& manager = m.objects(full_path.string());
+    BOOST_CHECK_EQUAL(manager.all().count(), 2);
+    */
+    /*
+    Informer m;
+    auto& informers = m.objects(full_path.string());
+    //informers.all();
+    BOOST_CHECK_EQUAL(informers.all().count(), 2);
+    */
+
+    //auto& informers = neutron::Informer::objects(full_path.string());
     //BOOST_CHECK_EQUAL(informers.all().count(), 2);
 
 }
--- processing/compute_informer_order/tests/basic/test_region.cpp
+++ processing/compute_informer_order/tests/basic/test_region.cpp
@@ -11,9 +11,10 @@ BOOST_AUTO_TEST_CASE(read)
 {
     namespace fs = boost::filesystem;
     fs::path full_path = test_data_dir / fs::path("region.tsv");
-
+    /*
     auto& region_manager = neutron::Region::objects(full_path.string());
     BOOST_CHECK_EQUAL(region_manager.all().count(), 1);
+    */
 }
 
 BOOST_AUTO_TEST_SUITE_END()
