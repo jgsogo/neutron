44c2dda82b6a4f52ee3970ba684645f45f49aa64
Javier G. Sogo <jgsogo@gmail.com>
2016-11-01 20:34:43 +0100

    base model for emails

--- webapp/server/admin.py
+++ webapp/server/admin.py
@@ -3,10 +3,17 @@
 
 
 from django.contrib import admin
-from server.models import Question
+from server.models import Question, Email
 
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('user', '__str__')
 
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'subject', 'sent', 'staff_recipient')
+    list_filter = ('sent',)
+
+
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Email, EmailAdmin)
---
+++ webapp/server/models/email.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage, get_connection
+from django.utils.timezone import now
+from django.conf import settings
+
+from model_utils.choices import Choices
+from jsonfield import JSONField
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class EmailManager(models.Manager):
+
+    def send(self, limit=None):
+        qs = self.filter(sent__isnull=True)[:limit]
+        with get_connection() as connection:
+            return sum(email.send(connection) for email in qs)
+
+
+class Email(models.Model):
+    STAFF_RECIPIENTS = Choices((0, 'managers', _("Managers")),
+                               (1, 'admins', _("Admins")))
+
+    subject = models.CharField(max_length=256)
+    recipient = models.EmailField()
+    staff_recipient = models.IntegerField(choices=STAFF_RECIPIENTS)
+
+    message = models.TextField()
+
+    json = JSONField()
+    template = models.CharField(max_length=60, blank=True, null=True)
+
+    sent = models.DateTimeField(blank=True, null=True)
+    fail_count = models.IntegerField(default=0)
+
+    @property
+    def is_sent(self):
+        return self.sent != None
+
+    @property
+    def text(self):
+        if not self.is_sent and self.template:
+            return render_to_string(self.template, self.json)
+        else:
+            return self.message
+
+    def send(self, connection=None):
+        # TODO: Get these data from settings
+        staff_recipients = []
+        from_email = None
+
+        text = self.text
+        obj = EmailMessage(self.subject, text,
+                           from_email=from_email,
+                           to=[self.recipient] + staff_recipients,
+                           connection=connection,
+                           )
+        try:
+            obj.send(fail_silently=False)
+            self.message = text
+            self.sent = now()
+            return 1
+        except Exception as e:
+            self.fail_count += 1
+            log.error("Error sending mail: {!r}".format(e))
+            return 0
+        finally:
+            self.save()
+
