7e8b90f8d7d13fc8f2629f78e62272e640c6e30d
Javier G. Sogo <jgsogo@gmail.com>
2016-12-12 14:53:11 +0100

    mods given in doc

--- webapp/server/views/about.py
+++ webapp/server/views/about.py
@@ -13,9 +13,13 @@ class AboutView(TemplateView):
         # Data about team members: name, image, linkedin, twitter
         team = [('María Varela', static('team/maria_varela.jpg'), 'https://es.linkedin.com/in/mariavarela', None),
                 ('Javier G. Sogo', static('team/javier_g_sogo.jpg'), 'https://es.linkedin.com/in/jgsogo', 'https://twitter.com/jgsogo'),
-                ('Alberto Gómez Font', static('team/alberto_gomez_font.jpg'), None, None),
-                ('Antonio Martín', static('team/antonio_martin.jpg'), None, None),
-                ('Miguel Varela', static('team/miguel_varela.jpg'), None, None),
+                ('Alberto Gómez Font', static('team/alberto_gomez_font.jpg'), 'https://www.linkedin.com/in/alberto-g%C3%B3mez-font-718a8224', 'https://twitter.com/GomezFont'),
+                ('Antonio Martín', static('team/antonio_martin.jpg'), 'https://www.linkedin.com/in/amoenus', 'https://twitter.com/_amoenus'),
+                ]
+
+        team2 = [('Miguel Varela', static('team/miguel_varela.jpg'), None, None),
+                 ('Elena Bazán', static('team/elena_bazan.jpg'), 'https://www.linkedin.com/in/elenabazan', None),
+                 ('Javier Calvo Martín', static('team/javier_calvo.jpg'), 'https://www.linkedin.com/in/javiercalvomartin', None),
                 ]
         shuffle(team)
-        return super(AboutView, self).get_context_data(team=team, **kwargs)
+        return super(AboutView, self).get_context_data(team=team, team2=team2, **kwargs)
